502e74eacf96cd29e3755f98dc82db76fe67f5b9
M. Sulaiman Javed <69959497+Sulaiman29@users.noreply.github.com>
2022-04-01 23:47:42 +0500

    App ver 1.0

---
+++ ABC_book/app/src/main/java/com/example/abc_book/Activity_A.kt
@@ -0,0 +1,11 @@
+package com.example.abc_book
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_A : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_a)
+    }
+}
